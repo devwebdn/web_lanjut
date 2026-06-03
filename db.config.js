98f2654d1b7140ac36fd1f57cbabb76bc828d9e4
dani <ahmaddani25.mhs@akb.ac.id>
2026-06-03 08:50:52 +0700

    Add files via upload

---
+++ db.config.js
@@ -0,0 +1,12 @@
+import { Sequelize } from "sequelize";
+ const db = new Sequelize('web_lanjut', 'root', '', {
+ host: "localhost",
+ dialect: "mysql",
+ "define": {
+ "timestamps": false
+ }
+ });
+ export default db;
+ (async()=>{
+     await db.sync();
+})();
---
+++ user.routes.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+import {
+  tambahuser,
+  login,
+} from "../controllers/user.controllers.js";
+
+import {
+  authenticateToken,
+} from "../middleware/VerifyTokens.js";
+
+const routerUser = express.Router();
+
+routerUser.post("/", tambahuser);
+
+routerUser.post("/login", login);
+
+routerUser.get(
+  "/dashboard",
+  authenticateToken,
+  (req, res) => {
+    res.send(
+      "Welcome to Dashboard"
+    );
+  }
+);
+
+export default routerUser;
